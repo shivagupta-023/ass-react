6390c18323ebf1f65064e412f087ee990415e9dc
Shiva Gupta <shivagupta2580g@gmail.com>
2025-08-27 23:22:23 +0530

    Update Input.jsx

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './Input.css';
+import './input.css';
 
-// हम label और required को अलग कर रहे हैं और बाकी सब कुछ ...props में डाल रहे हैं
+
 function Input({ label, required, ...props }) {
   return (
     <div className="input-group">
@@ -9,10 +9,6 @@ function Input({ label, required, ...props }) {
         {label}
         {required && <span className="required-star">*</span>}
       </label>
-      {/* यहाँ ...props बहुत ज़रूरी है। 
-        यह सुनिश्चित करता है कि आपके दिए गए सभी props (जैसे value, name, placeholder, onChange, type) 
-        सीधे असली <input> टैग पर लागू हो जाएं। यही आपकी टाइपिंग की समस्या को ठीक करेगा।
-      */}
       <input {...props} />
     </div>
   );

1c6063e148ab006a6ac651bea388b88f69e920cd
Shiva Gupta <shivagupta2580g@gmail.com>
2025-08-27 22:56:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 // import './App.css';
 import './AllApp.css'
-import Home from './pages/home';
+import Home from './pages/Home';
 import CreateAccountPage from './pages/CreateAccount/CreateAccount';
 import Login from './pages/login/Login'; 
 import AccountSettings from './pages/AccountSetting/AccountSetting';
